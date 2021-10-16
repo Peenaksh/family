96f463286059872f5534b68733137ab46bfe6eee
Peenaksh <85746966+Peenaksh@users.noreply.github.com>
2021-10-16 19:21:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+var reasons = [
+    "Dad",
+    "Me",
+    "Mom",
+    "Sister",
+    "Grandmother"
+  
+  ];
+  var images = [
+   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFWJW9f2df8ibmWPwTui-rpByNkBffoOJ80g&usqp=CAU",
+    "https://w7.pngwing.com/pngs/665/604/png-transparent-baby-boss-boy-character-illustration-the-boss-baby-brother-dreamworks-animation-film-the-boss-baby-child-toddler-boy.png",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6LcEVcyZ-Ien9ulvh-WBZrOTa6lm8gNnk6A&usqp=CAU",
+    "download.png",
+    "download.jpg"
+  ];
+  
+  var i = 0;
+  function nextslide() {  document.getElementById("reasontext").innerHTML = reasons[i];
+    document.getElementById("album").src = images[i];
+    i++;
+                        document.getElementById("audio").play();
+   
+  }
+  
+  
+  
+  
